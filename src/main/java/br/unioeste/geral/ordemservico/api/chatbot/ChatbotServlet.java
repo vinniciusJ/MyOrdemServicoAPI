94e48c8f3d5c890a9d98c12ea337b38231755db0
Vinicius de Oliveira Jimenez <vinicius.jimenez@itaipuparquetec.org.br>
2025-03-05 23:22:23 -0300

    feat: add chatbot

---
+++ src/main/java/br/unioeste/geral/ordemservico/api/chatbot/ChatbotServlet.java
@@ -0,0 +1,43 @@
+package br.unioeste.geral.ordemservico.api.chatbot;
+
+import br.unioeste.geral.ordemservico.bo.cliente.Cliente;
+import br.unioeste.geral.ordemservico.chatbot.service.UCChatbotServicos;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/chatbot")
+public class ChatbotServlet extends HttpServlet {
+    private final ObjectMapper objectMapper;
+    private final UCChatbotServicos chatbotServicos;
+
+    public ChatbotServlet(){
+        objectMapper = new ObjectMapper();
+        chatbotServicos = new UCChatbotServicos();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+
+        try{
+            String mensagem = request.getParameter("mensagem");
+            String resposta = chatbotServicos.enviarMensagem(mensagem);
+
+            response.getWriter().write(resposta);
+        }
+        catch (Exception e){
+            String errorJSON = objectMapper.writeValueAsString(e);
+
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println(errorJSON);
+        }
+    }
+}
